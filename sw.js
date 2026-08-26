dc3f64712e7007711f366d00f378809ae953a4f3
patrisanla <patri.fulca@gmail.com>
2026-08-26 18:43:00 +0200

    v4.0 f

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME='tecendo-nos-v47';
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  const req = event.request;
+  if (req.method !== 'GET') return;
+
+  event.respondWith(
+    fetch(req, {cache:'no-store'})
+      .then(resp => {
+        if (resp && resp.ok) {
+          const copy = resp.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(req, copy));
+        }
+        return resp;
+      })
+      .catch(() => caches.match(req))
+  );
+});
